94ad11d9e76c9925b57826e5ce344b890dd0e72d
NolanRberts <99685646+NolanRberts@users.noreply.github.com>
2025-09-28 12:40:17 -0400

    Task 1A Velocity

---
+++ Journal 3/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> asteroidTransforms;
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+    Vector3 vel, pos;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        pos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            vel.y += 0.05f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            vel.x -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vel.y -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            vel.x += 0.05f;
+        }
+
+        pos += vel;
+
+        transform.position = pos;
+
+        vel = Vector3.zero;
+    }
+
+}
