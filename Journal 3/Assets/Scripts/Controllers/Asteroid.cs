ec6db1ac5fbe6e320d5592ce41eb5a8f6db313d2
NolanRberts <99685646+NolanRberts@users.noreply.github.com>
2025-09-28 16:31:38 -0400

    Task 3

--- Journal 3/Assets/Scripts/Controllers/Asteroid.cs
+++ Journal 3/Assets/Scripts/Controllers/Asteroid.cs
@@ -8,13 +8,37 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    public Vector3 dir, next;
+    bool arrived;
+
     // Start is called before the first frame update
     void Start()
     {
+        moveSpeed = 0.01f;
+        maxFloatDistance = 3.2f;
+        arrivalDistance = 0.1f;
+        arrived = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (arrived)
+        {
+            next = new Vector3(this.transform.position.x + Random.Range(-maxFloatDistance, maxFloatDistance), this.transform.position.y + (Random.Range(-maxFloatDistance, maxFloatDistance)));
+            arrived = false;
+        }
+
+        if ( (next.x - this.transform.position.x <= arrivalDistance) && (next.y - this.transform.position.y <= arrivalDistance))
+        {
+            arrived = true;
+        }
+
+        dir = (next - this.transform.position).normalized;
+
+        dir.x *= moveSpeed;
+        dir.y *= moveSpeed;
+
+        transform.position += dir;
     }
 }
